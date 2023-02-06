781a3a2f2fbdd5aebd542ad012ed4bbb4b422c16
Mile Davidovic <mile.davidovic@live.com>
2023-02-06 22:54:26 +0100

    Fixed bloat.

--- sh/find_bloat.sh
+++ sh/find_bloat.sh
@@ -3,6 +3,7 @@ set -eu
 script_path=$( cd "$(dirname "${BASH_SOURCE[0]}")" ; pwd -P )
 cd "$script_path/.."
 
-cargo bloat --release --bin egui_demo_app -n 200 | rg "egui "
+cargo bloat --release --bin rser2tcp s-n 200 
+#| rg "eframe "
 
-cargo llvm-lines -p egui_demo_lib | rg egui | head -30
+cargo llvm-lines --bin rser2tcp | rg eframe | head -30
